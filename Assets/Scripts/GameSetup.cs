905f4d61c732125bc2ef81cbf183bf50b137f774
Taro Omiya <taro.omiya@omiyagames.com>
2015-12-12 13:35:11 -0500

    Limiting the connection to just 2 players.

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+public class GameSetup : MonoBehaviour
+{
+    void Awake()
+    {
+        // Only allow 2 players to connect
+        NetworkManager manager = GetComponent<NetworkManager>();
+        manager.maxConnections = 2;
+    }
+}
