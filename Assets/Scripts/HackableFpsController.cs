1384aaef14557b879df32bc579c4b51e4c12aeb9
Taro Omiya <taro.omiya@omiyagames.com>
2015-12-13 15:11:30 -0500

    Attempting to use the CharacterController instead.  Much more control over movement speeds.

--- Assets/Scripts/HackableFpsController.cs
+++ Assets/Scripts/HackableFpsController.cs
@@ -40,7 +40,7 @@ public class HackableFpsController : RigidbodyFirstPersonController
     {
         if ((player.CurrentActiveControls & PlayerSetup.ActiveControls.Run) != 0)
         {
-            return base.GetRunInput();
+            return CrossPlatformInputManager.GetButtonDown("Run");
         }
         else
         {
