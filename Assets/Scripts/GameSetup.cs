b3a05932d8d9bbb80d27f6a3e0aa8440f2de7b5e
Taro Omiya <taro.omiya@omiyagames.com>
2015-12-13 17:59:09 -0500

    Attempting to use layers to prevent bullets from getting stuck inside the shield.

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -5,6 +5,73 @@ using OmiyaGames;
 [RequireComponent(typeof(NetworkManager))]
 public class GameSetup : MonoBehaviour
 {
+    public const string playerBulletLayer = "Player Bullet",
+        oppositionBulletLayer = "Opposition Bullet",
+        neutralBulletLayer = "Neautral Bullet",
+        playerAvatarLayer = "Player Avatar",
+        oppositionAvatarLayer = "Opposition Avatar";
+
+    static int playerBulletLayerCache = -1,
+        oppositionBulletLayerCache = -1,
+        neutralBulletLayerCache = -1,
+        playerAvatarLayerCache = -1,
+        oppositionAvatarLayerCache = -1;
+
+    public static int playerBulletLayerInt
+    {
+        get
+        {
+            if(playerBulletLayerCache < 0)
+            {
+                playerBulletLayerCache = LayerMask.NameToLayer(playerBulletLayer);
+            }
+            return playerBulletLayerCache;
+        }
+    }
+    public static int oppositionBulletLayerInt
+    {
+        get
+        {
+            if (oppositionBulletLayerCache < 0)
+            {
+                oppositionBulletLayerCache = LayerMask.NameToLayer(oppositionBulletLayer);
+            }
+            return oppositionBulletLayerCache;
+        }
+    }
+    public static int neutralBulletLayerInt
+    {
+        get
+        {
+            if (neutralBulletLayerCache < 0)
+            {
+                neutralBulletLayerCache = LayerMask.NameToLayer(neutralBulletLayer);
+            }
+            return neutralBulletLayerCache;
+        }
+    }
+    public static int playerAvatarLayerInt
+    {
+        get
+        {
+            if (playerAvatarLayerCache < 0)
+            {
+                playerAvatarLayerCache = LayerMask.NameToLayer(playerAvatarLayer);
+            }
+            return playerAvatarLayerCache;
+        }
+    }
+    public static int oppositionAvatarLayerInt
+    {
+        get
+        {
+            if (oppositionAvatarLayerCache < 0)
+            {
+                oppositionAvatarLayerCache = LayerMask.NameToLayer(oppositionAvatarLayer);
+            }
+            return oppositionAvatarLayerCache;
+        }
+    }
     void Awake()
     {
         // Only allow 2 players to connect
