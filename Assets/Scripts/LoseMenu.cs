52ffe09f936fb2430f9a211f7651331ac0f4bb61
Taro Omiya <taro.omiya@omiyagames.com>
2015-12-14 10:52:12 -0500

    Changing the win/lose menus to not stop time, and only have one button available.

---
+++ Assets/Scripts/LoseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using OmiyaGames;
+
+public class LoseMenu : IMenu
+{
+    [SerializeField]
+    Button defaultButton;
+
+    public override GameObject DefaultUi
+    {
+        get
+        {
+            return defaultButton.gameObject;
+        }
+    }
+
+    public override Type MenuType
+    {
+        get
+        {
+            return Type.ManagedMenu;
+        }
+    }
+
+    public void OnQuitClicked()
+    {
+        NetworkManager.singleton.StopHost();
+        Manager.ButtonClick.Play();
+    }
+}
