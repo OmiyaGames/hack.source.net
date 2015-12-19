1390bd678c50560a98392b611160e10858835a66
Taro Omiya <taro.omiya@omiyagames.com>
2015-12-19 15:21:04 -0500

    Adding a tiny buffer placing the spot.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -13,13 +13,15 @@ public class Laser : MonoBehaviour
     float maxDistance = 100f;
     [SerializeField]
     Transform spot;
+    [SerializeField]
+    [Range(0f, 1f)]
+    float bufferDistance = 0.2f;
 
     Animator animator;
     LineRenderer renderer;
     Ray laserRay;
     RaycastHit info;
     Vector3 defaultPosition;
-    Vector3 raycastPosition;
 
     void Start()
     {
@@ -36,8 +38,7 @@ public class Laser : MonoBehaviour
         if(Physics.Raycast(laserRay, out info, maxDistance, mask) == true)
         {
             // Convert hit position to local position
-            raycastPosition = transform.InverseTransformPoint(info.point);
-            PositionEndPoint(ref raycastPosition);
+            PositionEndPoint(transform.InverseTransformPoint(info.point));
 
             // Indicate we hit the player if tagged as such
             animator.SetBool(SpottedBool, info.collider.CompareTag("Player"));
@@ -45,18 +46,22 @@ public class Laser : MonoBehaviour
         else
         {
             // Set end to default position
-            PositionEndPoint(ref defaultPosition);
+            PositionEndPoint(defaultPosition);
 
             // Indicate nothing was hit
             animator.SetBool(SpottedBool, false);
         }
     }
 
-    void PositionEndPoint(ref Vector3 position)
+    void PositionEndPoint(Vector3 position)
     {
-        raycastPosition.x = 0;
-        raycastPosition.y = 0;
-        renderer.SetPosition(1, raycastPosition);
-        spot.localPosition = raycastPosition;
+        // Position the end of the laser
+        position.x = 0;
+        position.y = 0;
+        renderer.SetPosition(1, position);
+
+        // Position the spot
+        position.z -= bufferDistance;
+        spot.localPosition = position;
     }
 }
