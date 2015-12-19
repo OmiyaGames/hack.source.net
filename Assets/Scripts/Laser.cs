5a0dab0b82b38ddd9580b62b30d7c6a6acda9d8e
Taro Omiya <taro.omiya@omiyagames.com>
2015-12-19 14:46:05 -0500

    Making attempts to add a laser.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask mask;
+    [SerializeField]
+    [Range(1f, 500f)]
+    float maxDistance = 100f;
+
+    LineRenderer renderer;
+    Ray laserRay;
+    RaycastHit info;
+    Vector3 defaultPosition;
+    Vector3 raycastPosition;
+
+    void Start()
+    {
+        renderer = GetComponent<LineRenderer>();
+        defaultPosition = new Vector3(0, 0, maxDistance);
+    }
+
+    void Update ()
+    {
+        // Raycast something
+        laserRay.origin = transform.position;
+        laserRay.direction = transform.forward;
+        if(Physics.Raycast(laserRay, out info, maxDistance, mask) == true)
+        {
+            // Convert hit position to local position
+            raycastPosition = transform.worldToLocalMatrix * info.point;
+            raycastPosition.x = 0;
+            raycastPosition.y = 0;
+            renderer.SetPosition(1, raycastPosition);
+        }
+        else
+        {
+            renderer.SetPosition(1, defaultPosition);
+        }
+    }
+}
